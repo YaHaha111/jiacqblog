739e016ef926feca7f5f61b206b61c1310a0cbaa
YaHaha111 <jcq522971257@gmail.com>
2020-12-22 15:16:32 +0800

    修改下

--- jiacqblog/jiacqblog/settings.py
+++ jiacqblog/jiacqblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6qc+ucg1v1t(t+j3+bxbvt6s1j^+4jt&sck5is8_$im!=hv&15'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
