dc25d06ef8dcb933d8dc334fe55f60ae9dbb7b91
YaHaha111 <jcq522971257@gmail.com>
2020-12-23 16:59:33 +0800

    userinfo

--- jiacqblog/userinfo/admin.py
+++ jiacqblog/userinfo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserIP, IP_comment, IP_reply
+
+
+
+
+class UserIPAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'address', 'name', 'email', 'first_time', 'last_time', 'views']
+    fields = ['ip', 'address', 'name','email']
+
+class IP_commentAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'like_comment', 'like_time']
+
+class IP_replyAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'like_reply', 'like_time']
+
+
+admin.site.register(UserIP, UserIPAdmin)
+admin.site.register(IP_comment, IP_commentAdmin)
+admin.site.register(IP_reply, IP_replyAdmin)
